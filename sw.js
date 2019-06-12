ed067fed155f404c3c0d50c6084d3d1e28ee19c7
spiray <spira.joe214@gmail.com>
2019-06-12 15:33:48 -0400

    Initial commit with ignored

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("ccx").then(cache => cache.addAll([ "/", "/index.html", "/style.css", "/helpers.js", "/main.js", "/manifest.json" ])));
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
