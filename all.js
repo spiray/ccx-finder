563ab33cd6134db24bc2805f03d6f6891b5427da
spiray <spira.joe214@gmail.com>
2019-06-18 17:24:25 -0400

    Added control for other BT

--- all.js
+++ all.js
@@ -6,6 +6,7 @@ var hlaGroups = ["0075904", "0075938", "0076000", "0076001", "0076011", "0076014
 var resultP = document.getElementById("result");
 var groupInput = document.getElementById("group-num");
 var id = document.getElementById("id");
+var nicknameSelect = document.getElementById("nickname-select");
 var comm = ["ASL", "AWH", "BAX", "BBB", "BBX", "BDW", "BEO", "BEW", "BFV", "BIQ", "BIR", "BIY", "BIZ", "BJB", "BLF", "BLT", "BNZ", "BVJ", "BVV", "BVY", "BWJ", "BYA", "BYE", "BYX", "BZP", "CBU", "CBZ", "CFS", "CSL", "DCU", "DCW", "DJF", "DJJ", "DMP", "DSX", "DTB", "DUG", "DWK", "DXR", "DYU", "EHO", "EON", "EWS", "EYL", "FEO", "FLP", "FOV", "FWQ", "GUL", "GWB", "HAA", "HAB", "HAC", "HAF", "HAM", "HAQ", "HAX", "HAZ", "HBD", "HBH", "HBK", "HBM", "HBX", "HBY", "HCD", "HCW", "HED", "HEF", "HEN", "HFM", "HFR", "HFW", "HHP", "HHW", "HJN", "HLM", "HLR", "HMB", "HPA", "HPF", "HPR", "HRB", "HSA", "HSD", "HSE", "HSF", "HSG", "HSH", "HSJ", "HSL", "HSQ", "HSU", "HTK", "HUF", "HVD", "HWA", "HWB", "HWE", "HWQ", "HWW", "HXS", "HYB", "IBB", "IIC", "IQZ", "IRA", "ISC", "ISS", "JDJ", "JDU", "JEE", "JEJ", "JFX", "JFY", "JGA", "JGB", "JGD", "JGE", "JGG", "JGH", "JGI", "JGJ", "JGL", "JGM", "JGN", "JGO", "JGP", "JGQ", "JGR", "JGS", "JGT", "JGU", "JGV", "JGW", "JGX", "JGY", "JGZ", "JVK", "JVK", "JVL", "JXW", "KBH", "KDJ", "KDP", "KDQ", "KEC", "KJP", "KKN", "KLX", "KPH", "KPU", "KQD", "KQL", "KQM", "KQS", "KUG", "KUK", "KWH", "KZR", "LDW", "LDY", "LES", "LGV", "LHG", "LKF", "LSH", "LSW", "LWR", "LZU", "MGZ", "MKV", "MKY", "MWJ", "MWK", "NBZ", "NCL", "NDD", "NEB", "NFC", "NFE", "NFW", "NHL", "NHV", "NHX", "NII", "NJD", "NJE", "NJI", "NJO", "NJP", "NJR", "NJS", "NJT", "NJX", "NKG", "NKH", "NKO", "NLD", "NLE", "NLG", "NLS", "NMO", "NMR", "NNI", "NOB", "NOC", "NOD", "NOV", "NPH", "NRQ", "NRW", "NSG", "NSI", "NSJ", "NSK", "NSY", "NTR", "NTW", "NUF", "NUH", "NVP", "NVY", "NYM", "ORN", "ORN", "ORZ", "ORZ", "OUI", "OYC", "OZC", "PFR", "PFZ", "PJY", "PKP", "PUB", "RFW", "RGW", "RJA", "RJU", "RJX", "RKU", "RKX", "RLX", "RQV", "RQV", "RUG", "RWM", "RWX", "RWY", "RXR", "RYJ", "RYJ", "SCV", "SDQ", "SGH", "SGX", "SGZ", "SLQ", "SMJ", "SNJ", "SOV", "SQW", "SRA", "SRD", "SUB", "SUC", "SUX", "SUY", "SUZ", "SXL", "TBG", "TEB", "TGW", "TJU", "TLH", "TMU", "TQL", "TQT", "TQW", "TUL", "TUU", "TUV", "TVV", "TWF", "TWP", "TYN", "TZF", "UAN", "UCJ", "UFD", "UGC", "UGY", "UIX", "UOG", "UOK", "UOQ", "UQW", "URG", "UWT", "UXI", "UXW", "VEG", "VGG", "WAY", "WRC", "WUG", "WUR", "WYE", "YHA", "YHB", "YHD", "YHE", "YHF", "YHG", "YHL", "YHN", "YHO", "YHP", "YHQ", "YHX", "YHY", "YKA", "YKC", "YKE", "YKF", "YKG", "YKH", "YKJ", "YKL", "YKQ", "YKR", "YKS", "YKT", "YKZ"];
 var med = ["NJH", "NJN", "YKB", "YKD", "YKI", "YKK", "YKM", "YKO", "YKU"];
 var HLA = "ZJC";
@@ -18,16 +19,16 @@ function setColor(color) {
 function showResult(value) {
   if (comm.includes(value)) {
     resultP.textContent = "Horizon Carecentrix Commercial";
-    id.textContent = "(1890)";
+    id.textContent = localStorage.nickname === "OHH" ? "(1890)" : "(4317)";
   } else if (med.includes(value)) {
     resultP.textContent = "Horizon Carecentrix Medicare Adv";
-    id.textContent = "(1892)";
+    id.textContent = localStorage.nickname === "OHH" ? "(1892)" : "(4319)";
   } else if (value.startsWith(FED)) {
     resultP.textContent = "Horizon Carecentrix FEP";
-    id.textContent = "(1891)";
+    id.textContent = localStorage.nickname === "OHH" ? "(1891)" : "(4318)";
   } else if (value === HLA) {
     resultP.textContent = "Horizon Carecentrix HLA";
-    id.textContent = "(1893)";
+    id.textContent = localStorage.nickname === "OHH" ? "(1893)" : "(4324)";
   } else {
     resultP.textContent = "Please enter a Group #";
     id.textContent = "";
@@ -67,7 +68,7 @@ function groupInputHandler(_ref2) {
 
   if (hlaGroups.includes(value)) {
     resultP.textContent = "Horizon Carecentrix HLA";
-    id.textContent = "(1893)";
+    id.textContent = localStorage.nickname === "OHH" ? "(1893)" : "(4324)";
   } else {
     resultP.textContent = "HORIZON OUT OF STATE - CARE CENTRIX PRIMARY";
     id.textContent = "(1060)";
@@ -76,6 +77,10 @@ function groupInputHandler(_ref2) {
   setColor("#228738");
 }
 
+if (localStorage.nickname) nicknameSelect.value = localStorage.nickname;
+nicknameSelect.addEventListener("change", function () {
+  localStorage.nickname = nicknameSelect.options[nicknameSelect.selectedIndex].value;
+});
 digitInput.addEventListener("change", digitInputHandler);
 groupInput.addEventListener("change", groupInputHandler);
 new ClipboardJS(".btn");
